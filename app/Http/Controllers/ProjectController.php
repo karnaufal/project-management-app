f5b9393cde5ee32e5ed52c10ef365a8661df592a
Naufal S <77094080+karnaufal@users.noreply.github.com>
2025-08-15 18:19:53 +0700

    Sumarry

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+    $projects = Auth::user()->projects;
+
+    return view('dashboard', compact('projects'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +10,7 @@ Route::get('/', function () {
 Route::get('/dashboard', [ProjectController::class, 'index']) 
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-    
-
+        
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -19,3 +18,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
